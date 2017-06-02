14bd09166cb7589240d37d2c528c6e0b2c3a1b05
Nguyễn Thành Đạt <thanhdat21293@gmail.com>
2017-06-02 10:19:41 +0700

    fix

--- views/layout.vue
+++ views/layout.vue
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-        <link rel="shortcut icon" type="image/x-icon" href="/public/img/color.">
+        <link rel="shortcut icon" type="image/x-icon" href="/public/img/color.ico">
         <link rel="stylesheet" type="text/css" href="/public/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="/public/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="/public/css/style.css">
